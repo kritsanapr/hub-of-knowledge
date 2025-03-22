a050450b8ab1c95103755f6ece74323d215ee0bb
kritsanapr <kritsana.pr.41@gmail.com>
2025-03-23 02:10:55 +0700

    feat: add admin page

---
+++ src/app/(admin)/certificate-management/[id]/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {
+    params: {
+        id: string
+    }
+}
+
+export default function UpdateCertificatePage({ params }: Readonly<Props>) {
+    return (
+        <div>UpdateCertificatePage</div>
+    )
+}
---
+++ src/app/(admin)/certificate-management/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function CertificateManagementPage() {
+    return (
+        <div className="flex flex-col gap-4">
+            <h1 className="text-2xl font-bold">Certificate Management</h1>
+        </div>
+    )
+}
